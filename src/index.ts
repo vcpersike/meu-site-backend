897bbacd890aa24f957de6fdcfc72875f96c652a
vcpersike <vcpersike@gmail.com>
2023-05-17 21:38:09 -0300

    Primeiros ajustes

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// import { NestFactory } from "@nestjs/core";
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import { AppModule } from "./app.module";
+// import * as functions from 'firebase-functions';
+// import * as express from 'express';
+
+// const server = express();
