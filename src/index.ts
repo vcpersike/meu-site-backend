df9a91e86df64672d1895d1bfef66dd036a14195
vcpersike <vcpersike@gmail.com>
2023-05-17 22:46:43 -0300

    Ajustes do firebase

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-// import { NestFactory } from "@nestjs/core";
-// import { ExpressAdapter } from '@nestjs/platform-express';
-// import { AppModule } from "./app.module";
-// import * as functions from 'firebase-functions';
-// import * as express from 'express';
-
-// const server = express();
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import { AppModule } from './app.module';
+const expressServer = express();
+const createFunction = async (expressInstance): Promise<void> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+};
+export const api = functions.https.onRequest(async (request, response) => {
+  await createFunction(expressServer);
+  expressServer(request, response);
+});
