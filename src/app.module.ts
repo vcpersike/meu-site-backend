ef10f9a3f852ea7deb7d166619ad271f81f65219
vcpersike <vcpersike@gmail.com>
2023-05-19 01:19:53 -0300

    ajuste de arquuivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GoogleSpreadsheetService } from './google-spreadsheet/google-spreadsheet.service';
 import { SpreadsheetController } from './google-spreadsheet/spreadsheet.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, SpreadsheetController],
-  providers: [AppService, GoogleSpreadsheetService],
+  controllers: [SpreadsheetController],
+  providers: [GoogleSpreadsheetService],
 })
 export class AppModule {}
